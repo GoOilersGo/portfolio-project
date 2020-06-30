89fc6df8e4c78c85ad53feec564520b42752391a
bob <bob@oilers.com>
2020-06-30 16:26:52 -0600

    Added boostrap and jobs are showing up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title=models.CharField(max_length=255)
+    image=models.ImageField(upload_to='images/')
+    body=models.TextField(max_length=200)
+    pub_date=models.DateTimeField()
+
+
+# Create a blog models
+#Needs a AuthenticationMiddleware
+#pub_date
+#body
+#Image
+
+
+#Add the Blog app to the settings
+#Create a migration
+#Do a migrate
+#Add to the admin
